158165f0d648218db6fb4656d1a4c7037558e10e
HingKosal <hingkosalup@gmail.com>
2020-07-22 19:51:45 +0700

    Static web

---
+++ Backend/templates/ProductDetailForm.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="PIXINVENT">
+    <title>Block UI - Modern Admin - Clean Bootstrap 4 Dashboard HTML Template + Bitcoin Dashboard</title>
+    <link rel="apple-touch-icon" href="../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
+          rel="stylesheet">
+    <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css"
+          rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/vendors.css">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN MODERN CSS-->
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/app.css">
+    <!-- END MODERN CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/core/colors/palette-gradient.css">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <!-- END Custom CSS-->
+</head>
+<body class="vertical-layout vertical-menu 2-columns   menu-expanded fixed-navbar"
+      data-open="click" data-menu="vertical-menu" data-col="2-columns">
+<!-- fixed-top-->
+<!--header start-->
+<?php include('../layouts/header.html') ?>
+<!--header end-->
+<!-- ////////////////////////////////////////////////////////////////////////////-->
+<!--sidebars start-->
+<?php include('../layouts/sidebars.html') ?>
+<!--sidebars end-->
+<!--content start-->
+<section id="horizontal-form-layouts" style="margin-left: 290px">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Add Product Detail</h2>
+                    <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                </div>
+                <div class="card-content collpase show">
+                    <div class="card-body">
+                        <form class="form form-horizontal">
+                            <div class="form-body">
+<!--                                <h4 class="form-section"><i class="ft-user"></i> Personal Info</h4>-->
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Product ID</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Product ID" name="pid">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Product Name</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Product Name" name="pname">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Brand</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Brand" name="brand">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Price</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Price" name="price">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Description</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Description" name="description">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Description</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Description" name="description">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row" >
+                                <label class="col-md-3 label-control" for="projectinput1"> Description</label>
+                                    <select class="custom-select col-md-2" id="quantityinput">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group row" >
+                                <label class="col-md-3 label-control" for="projectinput1"> Size</label>
+                                    <select class="custom-select col-md-2" id="sizeinput">
+                                        <option selected="">Small</option>
+                                        <option value="1">Medium</option>
+                                        <option value="2">Large</option>
+                                        <option value="3">X-large</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Company namme</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Company Name" name="cname">
+                                    </div>
+                                </div>
+
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Branch</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Branch" name="branch">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Company Address</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Company Address" name="caddress">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label class="col-md-3 label-control" for="projectinput1"> Phone</label>
+                                    <div class="col-md-5">
+                                        <input type="text" id="projectinput1" class="form-control" placeholder="Phone" name="phone">
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div class="form-actions">
+                                <button type="button" class="btn btn-warning mr-1">
+                                    <i class="ft-x"></i> Cancel
+                                </button>
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="la la-check-square-o"></i> Save
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--content end-->
+<!-- ////////////////////////////////////////////////////////////////////////////-->
+<!--footer start-->
+<?php include('../layouts/footer.html') ?>
+<!--footer end-->
+<!-- BEGIN VENDOR JS-->
+<script src="../app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+<!-- BEGIN VENDOR JS-->
+<!-- BEGIN PAGE VENDOR JS-->
+<!-- END PAGE VENDOR JS-->
+<!-- BEGIN MODERN JS-->
+<script src="../app-assets/js/core/app-menu.js" type="text/javascript"></script>
+<script src="../app-assets/js/core/app.js" type="text/javascript"></script>
+<script src="../app-assets/js/scripts/customizer.js" type="text/javascript"></script>
+<!-- END MODERN JS-->
+<!-- BEGIN PAGE LEVEL JS-->
+<script src="../app-assets/js/scripts/extensions/block-ui.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL JS-->
+</body>
+</html>
---
+++ Backend/templates/list.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
+    <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="PIXINVENT">
+    <title>Block UI - Modern Admin - Clean Bootstrap 4 Dashboard HTML Template + Bitcoin Dashboard</title>
+    <link rel="apple-touch-icon" href="../app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
+          rel="stylesheet">
+    <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css"
+          rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/vendors.css">
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN MODERN CSS-->
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/app.css">
+    <!-- END MODERN CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="../app-assets/css/core/colors/palette-gradient.css">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+    <!-- END Custom CSS-->
+</head>
+<body class="vertical-layout vertical-menu 2-columns   menu-expanded fixed-navbar"
+      data-open="click" data-menu="vertical-menu" data-col="2-columns">
+<!-- fixed-top-->
+<!--header start-->
+<?php include('../layouts/header.html') ?>
+<!--header end-->
+<!-- ////////////////////////////////////////////////////////////////////////////-->
+<!--sidebars start-->
+<?php include('../layouts/sidebars.html') ?>
+<!--sidebars end-->
+<!--content start-->
+<div class="content-body" style="margin-left: 280px">
+    <!-- Base style table -->
+    <section id="base-style">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h2 >Products List</h2>
+                        <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                        <div class="heading-elements">
+                        </div>
+                    </div>
+                    <div class="card-content collapse show">
+                        <div class="card-body card-dashboard">
+                            <a href="create.php" type="button" class="btn btn-primary"> Add New </a>
+                            <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-6"><div id="DataTables_Table_0_filter" class="dataTables_filter">
+                                            <label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="DataTables_Table_0"></label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12"><table class="table table-striped table-bordered base-style dataTable" id="DataTables_Table_0" role="grid" aria-describedby="DataTables_Table_0_info">
+                                            <thead>
+                                            <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending" style="width: 158.6px;">Name</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 248.2px;">Image</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 119.4px;">Price</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Age: activate to sort column ascending" style="width: 45px;">Quantity</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 101.8px;">Category</th><th class="sorting" tabindex="0" aria-controls="DataTables_Table_0" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 200px;">Action</th></tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr role="row" class="odd">
+                                                <td class="sorting_1">Crontop</td>
+                                                <td><img src=".../images/iphone.jpg"></td>
+                                                <td>19$</td>
+                                                <td>100</td>
+                                                <td>Clothes</td>
+                                                <td>
+                                                <a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="even">
+                                            <td class="sorting_1">Dell Vostro</td>
+                                                <td>N/A</td>
+                                                <td>900$</td>
+                                                <td>99</td>
+                                                <td>Mobile/Laptop</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="odd">
+                                            <td class="sorting_1">Sumsang Galaxy X10</td>
+                                                <td>N/A</td>
+                                                <td>890$</td>
+                                                <td>50</td>
+                                                <td>Mobile/Laptop</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="even">
+                                            <td class="sorting_1">Crontop</td>
+                                                <td>N/A</td>
+                                                <td>19$</td>
+                                                <td>100</td>
+                                                <td>Clothes</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="odd">
+                                            <td class="sorting_1">Sneaker</td>
+                                                <td>N/A</td>
+                                                <td>19$</td>
+                                                <td>2000</td>
+                                                <td>Shoes</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="even">
+                                            <td class="sorting_1">Crontop</td>
+                                                <td>N/A</td>
+                                                <td>19$</td>
+                                                <td>100</td>
+                                                <td>Clothes</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="odd">
+                                            <td class="sorting_1">Skirt</td>
+                                                <td>N/A</td>
+                                                <td>10$</td>
+                                                <td>350</td>
+                                                <td>Clothes</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="even">
+                                            <td class="sorting_1">Crontop</td>
+                                                <td>N/A</td>
+                                                <td>29$</td>
+                                                <td>100</td>
+                                                <td>Clothes</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="odd">
+                                            <td class="sorting_1">Boots</td>
+                                                <td>N/A</td>
+                                                <td>25$</td>
+                                                <td>200</td>
+                                                <td>Shoes</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr><tr role="row" class="even">
+                                            <td class="sorting_1">Iphone8</td>
+                                                <td>N/A</td>
+                                                <td>1050$</td>
+                                                <td>1500</td>
+                                                <td>Mobile/Laptop</td>
+                                                <td><a class="btn btn-sm btn-info"><i class="la la-pencil"></i></a>
+                                                <a class="btn btn-sm btn-danger"><i class="la la-trash-o"></i></a></td>
+                                            </tr></tbody>
+                                            <tfoot>
+                                            <tr><th rowspan="1" colspan="1">Name</th><th rowspan="1" colspan="1">Image</th><th rowspan="1" colspan="1">Category</th><th rowspan="1" colspan="1">Price</th><th rowspan="1" colspan="1">Quantity</th><th rowspan="1" colspan="1">Action</th></tr>
+                                            </tfoot>
+                                        </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="DataTables_Table_0_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="DataTables_Table_0_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="DataTables_Table_0_previous"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item "><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="2" tabindex="0" class="page-link">2</a></li><li class="paginate_button page-item "><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="3" tabindex="0" class="page-link">3</a></li><li class="paginate_button page-item "><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="5" tabindex="0" class="page-link">5</a></li><li class="paginate_button page-item "><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="6" tabindex="0" class="page-link">6</a></li><li class="paginate_button page-item next" id="DataTables_Table_0_next"><a href="#" aria-controls="DataTables_Table_0" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--/ Base style table -->
+</div>
+
+<!--content end-->
+<!-- ////////////////////////////////////////////////////////////////////////////-->
+<!--footer start-->
+<?php include('../layouts/footer.html') ?>
+<!--footer end-->
+<!-- BEGIN VENDOR JS-->
+<script src="../app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+<!-- BEGIN VENDOR JS-->
+<!-- BEGIN PAGE VENDOR JS-->
+<!-- END PAGE VENDOR JS-->
+<!-- BEGIN MODERN JS-->
+<script src="../app-assets/js/core/app-menu.js" type="text/javascript"></script>
+<script src="../app-assets/js/core/app.js" type="text/javascript"></script>
+<script src="../app-assets/js/scripts/customizer.js" type="text/javascript"></script>
+<!-- END MODERN JS-->
+<!-- BEGIN PAGE LEVEL JS-->
+<script src="../app-assets/js/scripts/extensions/block-ui.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL JS-->
+</body>
+</html>
